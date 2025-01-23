5ec02cbb4ab079683715da51131a7b66d0ac61e9
shk0349 <shk0349@naver.com>
2025-01-23 10:19:50 +0900

    initial ver.1.2

--- Detect_Boot/src/main/java/edu/mbc/Detect_Boot/Detect_Boot/DetectBootApplication.java
+++ Detect_Boot/src/main/java/edu/mbc/Detect_Boot/Detect_Boot/DetectBootApplication.java
@@ -1,13 +1,13 @@
-package edu.mbc.Float_Detect.Detect;
+package edu.mbc.Detect_Boot.Detect_Boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DetectApplication {
+public class DetectBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DetectApplication.class, args);
+		SpringApplication.run(DetectBootApplication.class, args);
 	}
 
 }
--- Detect_Boot/src/test/java/edu/mbc/Detect_Boot/Detect_Boot/DetectBootApplicationTests.java
+++ Detect_Boot/src/test/java/edu/mbc/Detect_Boot/Detect_Boot/DetectBootApplicationTests.java
@@ -1,10 +1,10 @@
-package edu.mbc.Float_Detect.Detect;
+package edu.mbc.Detect_Boot.Detect_Boot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DetectApplicationTests {
+class DetectBootApplicationTests {
 
 	@Test
 	void contextLoads() {
