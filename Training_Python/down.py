1a72144ee5f49fbff91e9ecd92c52b7def256196
918 <shk0349@naver.com>
2025-02-23 19:31:41 +0900

    최중 수정 ver.

--- Training_Python/down.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="NRW0ztrXrPYGaSXU6FAl")
-project = rf.workspace("nomore-underfitting").project("detect_float_waste_123")
-version = project.version(2)
-dataset = version.download("yolov8")
